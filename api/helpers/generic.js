5d19bd29ec8eaf15114d29160f145dbf75db63b0
Alex <alexlong2001@outlook.com>
2024-09-05 15:49:39 +0100

    fixed no token issue

--- api/helpers/generic.js
+++ api/helpers/generic.js
@@ -47,8 +47,11 @@ const getUserFromToken = (req, res, next) => {
   const token = req.header("Authorization");
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+    }
+
     next();
   } catch (error) {
     return res.status(403).json({ message: error });
